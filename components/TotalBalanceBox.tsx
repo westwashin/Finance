14322043489da1839101f404416af8fa8a1a06c3
westwashin <61764924+westwashin@users.noreply.github.com>
2024-07-24 21:34:06 +0700

    add components

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,4 +1,5 @@
 import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({
     accounts = [] , totalBanks, totalCurrentBalance
@@ -6,7 +7,7 @@ const TotalBalanceBox = ({
   return (
     <section className="total-balance">
         <div className="total-balance-chart">
-            {/*DoughnutChart */}
+            <DoughnutChart accounts={accounts}/>
         </div>
 
         <div className="flex flex-col gap-6">
