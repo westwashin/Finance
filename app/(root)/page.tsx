b60009d9ab8ef5fa23710da429b6f36cd6937d4b
westwashin <61764924+westwashin@users.noreply.github.com>
2024-07-25 19:06:40 +0700

    fix error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -29,7 +29,7 @@ const Home = () => {
         <RightSidebar 
           user={loggedIn}
           transactions={[]}
-          banks={[{},{}]}
+          banks={[{ currentBalance:123.50},{currentBalance:500.00}]}
         />
     </section>
   )
