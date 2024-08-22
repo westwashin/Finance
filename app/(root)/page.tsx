eaaca0465b0643980720fd5b20d8a22c625830f7
westwashin <61764924+westwashin@users.noreply.github.com>
2024-08-22 14:33:06 +0700

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,52 +1,55 @@
 import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox'
+// import RecentTransactions from '@/components/RecentTransactions';
 import RightSidebar from '@/components/RightSidebar';
-import { getLoggedInUser } from '@/lib/actions/user.actions';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = async ({ searchParams: {id, page}}: SearchParamProps) => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
-  const accounts = await getAccounts({userId: loggedIn.$id})
+  const accounts = await getAccounts({ 
+    userId: loggedIn.$id 
+  })
 
   if(!accounts) return;
+  
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
-  const accountsData = accounts?.data
-
-  const appwriteItemId = (id as string) || accounts?.data[0]?.appwriteItemId;
-
-  const account = await getAccount({appwriteItemId})
-
-  console.log({
-    accountsData,
-    account 
-  })
+  const account = await getAccount({ appwriteItemId })
 
   return (
     <section className="home">
-        <div className="home-content">
-            <header className="home-header">
-                <HeaderBox 
-                  type="greeting"
-                  title="welcome"
-                  user={loggedIn?.firstName || 'Guest'}
-                  subtext="Access and manage your Account"
-                />
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
 
-                <TotalBalanceBox 
-                accounts={accountsData}
-                totalBank={accounts?.totalBanks}
-                totalCurrentBalance={accounts?.totalCurrentBalance}
+          <TotalBalanceBox 
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
+          />
+        </header>
 
-                />
-            </header>
-          RECENT TRANS
-        </div>
+        {/* <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        /> */}
+      </div>
 
-        <RightSidebar 
-          user={loggedIn}
-          transactions={[accounts?.transactions]}
-          banks={accountsData?.slice(0, 2)}
-        />
+      <RightSidebar 
+        user={loggedIn}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      />
     </section>
   )
 }
