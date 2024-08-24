2fdae4fdd8819a69a7cee28207f32d37c3a520aa
westwashin <61764924+westwashin@users.noreply.github.com>
2024-08-24 14:27:12 +0700

    add copy

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,11 +1,12 @@
-
 import { formatAmount } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Copy from './Copy'
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
 
+  console.log(account);
   return (
     <div className="flex flex-col">
       <Link href={`/transaction-history/?id=${account.appwriteItemId}`} className="bank-card">
